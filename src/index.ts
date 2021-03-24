1d4e82e4e924ced7050bcaa9e1b1d67322f72bdb
Lucas Vieira <lucas@devplace.com.br>
2021-03-24 17:32:53 -0300

    electron-poc

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as Store from 'electron-store';
+
+const store = new Store({
+  migrations: {
+    '0.0.1': store => {
+      store.set('debugPhase', true);
+    },
+    '1.0.0': store => {
+      store.delete('debugPhase');
+      store.set('phase', '1.0.0');
+    },
+    '1.0.2': store => {
+      store.set('phase', '1.0.2');
+    },
+    '>=2.0.0': store => {
+      store.set('phase', '>=2.0.0');
+    },
+  },
+});
+
+console.log("LOG ~ store.get('phase')", store.get('phase'));
